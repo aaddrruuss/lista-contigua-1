47720b1efe196bffb40ae4202b5d189ede078321
Adrian Perez Masar <permasadrian@gmail.com>
2025-02-17 14:19:19 +0100

    Added headers

---
+++ src/ListaContigua.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ListaContigua.h"
+
+ListaContigua constructor(ListaContigua lista){
+    lista.n = 0;
+    lista.array = (int)calloc(lista.n, sizeof(int));
+    lista.capacidad = 0;
+}
+
+void destructor(ListaContigua lista){
+    free(lista.array);
+}
+
+int isLlena(ListaContigua lista){
+    int elementos = sizeof(lista.array) / sizeof(lista.array[0]);
+    if(elementos == lista.n){
+        return 1;
+    }
+    return 0;
+}
+
+int elementoPosicion(ListaContigua lista, int posicion){
+    assert(lista.array != NULL);
+
+}
+
+void modificarElemento(ListaContigua lista);
+
+void ampliarCapacidad(ListaContigua lista, int nuevaCapacidad);
+
+void insertarElemento(ListaContigua lista, int elemento);
+
+void eliminarUltimoElemento(ListaContigua lista);
