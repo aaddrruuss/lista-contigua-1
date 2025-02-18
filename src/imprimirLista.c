b10482af61be212a9e51ec3918cc96f9d33225eb
Adrián Pérez Masar <permasadrian@gmail.com>
2025-02-18 12:36:21 +0100

    Agregado biblioteca para imprimir la lista contigua

---
+++ src/imprimirLista.c
@@ -0,0 +1,8 @@
+#include "imprimirLista.h"
+
+void imprimirLista(ListaContigua lista){
+    printf("n = %d | Max = %d | ListaContigua: ", lista.n, lista.capacidad);
+    for(int i=0;i<lista.n;i++){
+        printf("%d", lista.array[i]);
+    }
+}
---
+++ src/imprimirLista.h
@@ -0,0 +1,7 @@
+#include "ListaContigua.h"
+#ifndef IMPRIMIR_LISTA_H
+#define IMPRIMIR_LISTA_H
+
+void imprimirLista(ListaContigua lista);
+
+#endif //IMPRIMIR_LISTA_H
