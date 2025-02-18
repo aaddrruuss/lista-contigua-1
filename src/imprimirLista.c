9fe169e52fe8113bc8ab36e7c637230b5b97f289
Adrián Pérez Masar <permasadrian@gmail.com>
2025-02-18 12:41:22 +0100

    Creado el codigo compilado, generada lista vacia en el main y modificado imprimirLista.c un poco

--- src/imprimirLista.c
+++ src/imprimirLista.c
@@ -3,7 +3,11 @@
 
 void imprimirLista(ListaContigua lista){
     printf("n = %d | Max = %d | ListaContigua: ", lista.n, lista.capacidad);
-    for(int i=0;i<lista.n;i++){
-        printf("%d", lista.array[i]);
+    if(lista.n!=0){
+        for(int i=0;i<lista.n;i++){
+            printf("%d", lista.array[i]);
+        }
+    }else{
+        printf("NULL");
     }
 }
--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "imprimirLista.h"
+#include "ListaContigua.h"
+
+int main(){
+    ListaContigua lista;
+    constructor(&lista);
+    imprimirLista(lista);
+
+    return 0;
+}
